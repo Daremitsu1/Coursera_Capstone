960ca2c39ee3a8fc942a1245ab2e6924aa9d4b55
Daremitsu1 <54842807+Daremitsu1@users.noreply.github.com>
2019-11-29 23:35:44 +0530

    Add files via upload

---
+++ Coursera_Capstone.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # This notebook was created as a requirement of the Applied Data Science Capstone peer graded assignment
+
+# ## Import the required libraries¶
+
+# In[ ]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# Print the following the statement: Hello Capstone Project Course!
+
+# In[1]:
+
+
+print("Hello Capstone Project Course!")
+
